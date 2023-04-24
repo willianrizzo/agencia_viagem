c3ac2efd0156814770e9606da37d44beb257489d
willrizzo <willianrizzoratao10@gmail.com>
2023-04-24 15:52:32 -0300

    Section terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './Componentes/Header'
+import Section from './Componentes/Section'
 
 function App() {
   return (
     <>
       <div>
         <Header/>
+        <Section/>
       </div>
        
     </>
--- src/Componentes/Section.jsx
+++ src/Componentes/Section.jsx
@@ -2,8 +2,14 @@ import Mala from '../assets/mala.jpg'
 
 function Section(){
     return(
-        <div className="bg-slate-800 w-full h-auto m-auto">
-            <img className='w-1/4 h-1/4' src={Mala} alt="" />
+        <div className="w-full h-auto flex my-10 items-center">
+            <img className='w-1/4 h-1/4  m-auto' src={Mala} alt="" />
+            <div className='w-1/2'>
+                <h3 className='w-5/6 text-center font-medium text-4xl'>Encontre aqui a viagem dos seus sonhos!!</h3>
+                <br />
+                <br />
+                <p className='w-5/6 text-xl text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro sapiente qui, dolore sint, earum assumenda temporibus illo quae pariatur aliquid cumque delectus obcaecati accusantium. Impedit a at quisquam ullam suscipit?Dolor modi facere fugiat consectetur.*</p>
+            </div>
             
         </div>
     )
