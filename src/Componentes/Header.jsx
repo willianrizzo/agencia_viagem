74b45c778fb38eb4568836aab042efa9afc2113d
willrizzo <willianrizzoratao10@gmail.com>
2023-04-24 17:24:34 -0300

    finalizando

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,16 +1,17 @@
 import Logo from '../assets/viagens.jpg'
 import Lupa from '../assets/lupa.png'
+import { Link } from 'react-router-dom'
 
 function Header(){
     return(
         <div className=""> 
-            <div className=' w-9/12 m-auto flex gap-10 items-center '>
-                <img className='w-32 h-32 mx-12' src={Logo} alt="" />   
-                <p className='font-normal text-lg hover:scale-x-110 duration-100'>Home</p>
-                <p className='font-normal text-lg hover:scale-x-110 duration-100'>Grand Canyon</p>
-                <p className='font-normal text-lg hover:scale-x-110 duration-100'>Escócia</p>
-                <p className='font-normal text-lg hover:scale-x-110 duration-100'>Muralhas da China</p>
-                <p className='font-normal text-lg hover:scale-x-110 duration-100'>Aruba</p>
+            <div className=' justify-center flex gap-10 items-center '>
+                <img className='h-32 mx-12' src={Logo} alt="" />   
+                <Link to='/' className='font-normal text-lg hover:scale-x-110 duration-100'>Home</Link>
+                <Link to='/canyon'  className='font-normal text-lg hover:scale-x-110 duration-100'>Grand Canyon</Link>
+                <Link to='/escocia' className='font-normal text-lg hover:scale-x-110 duration-100'>Escócia</Link>
+                <Link to='/china' className='font-normal text-lg hover:scale-x-110 duration-100'>Muralhas da China</Link>
+                <Link to='/aruba' className='font-normal text-lg hover:scale-x-110 duration-100'>Aruba</Link>
                 <input className='bg-gray-200 rounded-2xl p-1' type="search" />
                 <img className='w-8 h-8' src={Lupa} alt="" />
             </div>
