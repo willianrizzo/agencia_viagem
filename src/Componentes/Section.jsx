13a44e8a463fa335b644ddab62729fae013bd76a
willrizzo <willianrizzoratao10@gmail.com>
2023-04-24 15:33:08 -0300

    section 1

--- src/Componentes/Section.jsx
+++ src/Componentes/Section.jsx
@@ -1,7 +1,10 @@
+import Mala from '../assets/mala.jpg'
+
 function Section(){
     return(
-        <div>
-
+        <div className="bg-slate-800 w-full h-auto m-auto">
+            <img className='w-1/4 h-1/4' src={Mala} alt="" />
+            
         </div>
     )
 }
